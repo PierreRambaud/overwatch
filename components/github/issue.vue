8e9bc8eefd312327f4a91cda05f241342d48307c
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-09-04 22:33:27 +0200

    Add target blank

--- components/github/issue.vue
+++ components/github/issue.vue
@@ -1,6 +1,6 @@
 <template>
     <li class="list-group-item js-notification js-navigation-item unread issue-notification" aria-selected="false">
-        <a class="list-group-item-link" :href="issue.html_url" :title="issue.title">
+        <a class="list-group-item-link" :href="issue.html_url" :title="issue.title" target="_blank">
             <span :class="[iconState]" class="type-icon tooltipped tooltipped-w">
                 <svg v-if="isIssue" class="octicon octicon-issue-opened" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true">
                     <path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"/>
